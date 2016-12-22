9ee1f855ec76eff2a49b7b1c2e9d20f6eac2f09b
Damian Jedynak <djedynak90@gmail.com>
2016-12-23 00:18:03 +0100

    add new test for json validation in response

--- src/test/java/controller/LibraryControllerTest.java
+++ src/test/java/controller/LibraryControllerTest.java
@@ -4,6 +4,7 @@ import config.AppTestInitializer;
 import config.WebTestConfiguration;
 import dao.BookDAO;
 import model.Book;
+import org.hamcrest.core.IsNull;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,7 +21,9 @@ import service.LibraryCatalogService;
 
 import java.nio.charset.Charset;
 
+import static org.hamcrest.core.Is.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -46,16 +49,28 @@ public class LibraryControllerTest {
     @Before
     public void setup() throws Exception {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        Mockito.when(bookTestDAO.getBookByName("LOTR")).thenReturn(new Book("LOTR", "LOTR"));
+        Mockito.when(libraryCatalogService.checkOut("LOTR")).thenReturn(new Book("LOTR", "LOTR"));
 
     }
 
     @Test
     public void should_return_status_ok_when_send_request_for_existing_book() throws Exception {
-        Mockito.when(bookTestDAO.getBookByName("LOTR")).thenReturn(new Book("LOTR", "LOTR"));
-        Mockito.when(libraryCatalogService.checkOut("LOTR")).thenReturn(new Book("LOTR", "LOTR"));
-
         mockMvc.perform(get("/LOTR")
                 .contentType(contentType))
                 .andExpect(status().isOk());
     }
+
+    @Test
+    public void should_return_status_ok_and_correct_json_response_when_send_request_for_existing_book() throws Exception {
+        mockMvc.perform(get("/LOTR")
+                .contentType(contentType))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.isbn", is(0)))
+                .andExpect(jsonPath("name", is("LOTR")))
+                .andExpect(jsonPath("authorName", is("LOTR")))
+                .andExpect(jsonPath("checkedOut", is(false)))
+                .andExpect(jsonPath("user").value(IsNull.nullValue()));
+    }
+
 }
